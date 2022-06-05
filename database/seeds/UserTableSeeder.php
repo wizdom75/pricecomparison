2bb10b9b723e2dda4aa3c88084fdcd1ffc4fa7b6
Peter Ncube <peter@checkboard.com>
2022-06-05 18:22:39 +0100

    Fixed seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,7 +13,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        User::insertIgnore([
+        User::insertOrIgnore([
             'name'      => 'admin',
             'email'     => 'admin@lowprices4u.co.uk',
             'password'  => Hash::make('password'),
